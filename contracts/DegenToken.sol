c8264aa43f9f7e828db3c8cc17311e9d7b54a5aa
manojks1 <155665470+manojks1@users.noreply.github.com>
2024-01-21 11:38:41 +0530

    Update DegenToken.sol

--- contracts/DegenToken.sol
+++ contracts/DegenToken.sol
@@ -3,12 +3,38 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract DegenToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable, ERC20Burnable {
+    string public store;
 
-    constructor() ERC20("Degen", "DGN") {}
-
-        function mint(address to, uint256 amount) public onlyOwner {
-            _mint(to, amount);
+    constructor(address initialOwner) ERC20("Degen", "DGN") Ownable(initialOwner) {
+         store = "The Official Degen store has the following redeemable items: 1.  Lamp 2.  Toy 3. Car ";
     }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+       function redeem(uint256 choice,bool _store) public  {
+        require(_store==true,"Make sure you have read the item lists available in our store");
+        if (balanceOf(msg.sender) < 500) {
+            revert("Insufficient Balance");
+        }
+        assert(choice >= 1 && choice <= 3);
+        _burn(msg.sender, choice * 100);
+       
+    }
+          function Donate() public {
+        
+        require(balanceOf(msg.sender) >= 50, "Insufficient Tokens for Donation");
+        _burn(msg.sender, 50);
+        
+        emit DonationMade("You made a donation!");
+    }
+    event DonationMade(string message);
 }
+
+    
+
+   
